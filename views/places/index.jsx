422106b8cf0d008cc1cbcba518e9c8a761bde757
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-10-22 13:56:42 -0400

    safety commit

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -13,7 +13,7 @@ function index (data) {
     return (
         <Def>
             <main>
-                <h1>PLACES INDEX PAGE</h1>
+                <h1>Places to Rant or Rave About</h1>
                 {placesFormatted}
             </main>
         </Def>
