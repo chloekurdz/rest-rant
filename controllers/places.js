1a1b65effbe240cbc48e69bea176810daf886cc7
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-10-24 19:19:54 -0400

    jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -17,8 +21,9 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
+router.post('/', (req, res) => {
+    res.send("POST /places stub")
 })
+
 
 module.exports = router
