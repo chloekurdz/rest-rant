56cdbd0ffa7b57d0f721f6644fbb2a4a66993dd9
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-10-24 19:47:24 -0400

    fixed errors

--- controllers/places.js
+++ controllers/places.js
@@ -6,19 +6,6 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'Arizona',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-    }]
     res.render('places/index', { places })
 })
 
