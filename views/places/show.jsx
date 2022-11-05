7329e495a987fe2780622872074e7278c0360ec6
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-11-04 22:43:34 -0400

    added rant function and comment section

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,31 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    let mesage = ''
-    if (data.message) {
-        message = (
-            <h4 className="alert-danger">
-                {data.message}
-            </h4>
-        )
-    }
-    return (
-        <Def>
-            <main>
-                <h1>{ data.place.name }</h1>
-                {message}
-            </main>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                Edit
-            </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                <button type="submit" className="btn btn-danger">
-                Delete
-                </button>
-            </form>
-        </Def>
-        
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
     )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant!': 'Rave!'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -37,11 +31,7 @@ function show (data) {
                             Located in {data.place.city}, {data.place.state}
                         </h3>
                     </div>
-                    <div className="col-smm-6">
-                        <img src={data.place.pic} alt={data.place.name} />
-                        <h3>
-                            Located in {data.place.city}, {data.place.state}
-                        </h3>
+                    <div className="col-sm-6">
                         <h2>
                             Description
                         </h2>
@@ -49,15 +39,37 @@ function show (data) {
                             {data.place.showEstablished()}
                         </h3>
                         <h4>
-                            Serving {data.place.cuisnes}
+                            Serving {data.place.cuisines}
                         </h4>
                     </div>
+                    <div className="col-sm-6">
+                        <h2>Comments</h2>
+                        { comments }
+                        
+                    </div>
+                    <form action={`/places/${data.place.id}/comment`} method="POST">
+                    <div className="form-group">
+                        <label htmlFor="author">Author</label>
+                        <input className="form-control" id="author" name="author"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="content">Content</label>
+                        <input className="form-control" id="content" name="content" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="stars">Star Rating</label>
+                        <input className="form-control" id="stars" name="stars" type="number" />
+                    </div>
+                    <div>
+                        <label htmlFor="rant">Rant</label>
+                        <input id="rant" name="rant" type="checkbox" />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Submit" />
+                    </form>
                 </div>
             </main>
         </Def>
     )
 }
-
-
 
 module.exports = show
